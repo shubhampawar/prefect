e2314af5b3b3757b57180c9cff0e3d2dd0d7a72b
Shubham Pawar <shubham19954@gmail.com>
2024-06-20 16:12:09 +0530

    Update flow_demo.py

--- flow_demo.py
+++ flow_demo.py
@@ -1,12 +1,12 @@
 import os
 import asyncio
 from prefect import flow, task
-
+import time
 # Define asynchronous tasks with appropriate delays
 @flow
-async def do_something_important(duration: int, not_so_secret_value: str) -> None:
+def do_something_important(duration: int, not_so_secret_value: str) -> None:
     print(f"Sleeping for {duration} seconds")
-    await asyncio.sleep(duration)
+    time.sleep(duration)
     print(f"Doing something important with {not_so_secret_value} after {duration} seconds!")
 
 # Define the flow
@@ -22,15 +22,15 @@ async def some_work():
         raise ValueError("You forgot to set MY_NOT_SO_SECRET_CONFIG!")
 
     # Create a list of tasks with different sleep durations
-    tasks = [
-        do_something_important(5, not_so_secret_value),
-        do_something_important(4, not_so_secret_value),
-        do_something_important(3, not_so_secret_value),
-        do_something_important(2, not_so_secret_value)
-    ]
+
+    do_something_important(5, not_so_secret_value),
+    do_something_important(4, not_so_secret_value),
+    do_something_important(3, not_so_secret_value),
+    do_something_important(2, not_so_secret_value)
+
 
     # Run all tasks concurrently
-    await asyncio.gather(*tasks)
+    # await asyncio.gather(*tasks)
 
 # Run the flow
 if __name__ == "__main__":
